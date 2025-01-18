5346d92054cb15fdd38141bb6951a69274f1d405
Qmxhbms1 <alephnull072@gmail.com>
2025-01-18 15:51:21 +0100

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const CONFIG = {
 	// └─┘┴ ┴└─┘┴└─┘└─┘
 
 	// General
-	name: 'John',
+	name: 'Qmxhbms1',
 	imageBackground: false,
 	openInNewTab: true,
 	twelveHourFormat: false,
@@ -32,8 +32,8 @@ const CONFIG = {
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
 	trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-	defaultLatitude: '37.775',
-	defaultLongitude: '-122.419',
+	defaultLatitude: '44.81778',
+	defaultLongitude: '20.45694',
 
 	// Autochange
 	autoChangeTheme: true,
